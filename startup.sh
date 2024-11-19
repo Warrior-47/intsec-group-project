e4fbd1a9dc1f7690c0d02bbabf396eae8c5ae6f8
Sakib Uddin Tapu <59693766+Warrior-47@users.noreply.github.com>
2024-11-19 17:03:19 +0100

    feat: [tapu] added startup.sh script for automation of image and container creation

--- startup.sh
+++ startup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-# TO BE IMPLEMENTED
+DOCKER_IMAGE="samuelegarzon/group39_intsec:latest"
+GROUP_SECRET="4d852eea1a2dce9ea7f1222f56e3b2e0c9b05224"
+
+docker build -t $DOCKER_IMAGE .
+
+docker run -e GROUP_SECRET=$GROUP_SECRET -d --name passoire -p 8080:80 -p 8081:22 -p 3002:3002 -e HOST="localhost" $DOCKER_IMAGE
