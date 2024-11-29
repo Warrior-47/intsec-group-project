f5a254f2844a99246e28545dfd2fc984c41b17d7
Samuele Garzon <samuele.garzon@studenti.unitn.it>
2024-11-29 08:52:03 +0100

    [Samuele] added the pipe operator to the sanitize function in the cryptohelper server

--- passoire/crypto-helper/server.js
+++ passoire/crypto-helper/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 function sanitize(str){
-  return str.replace( /[";'\[\]\{}()<>&/]*/g,"")
+  return str.replace( /[";'\[\]\{}()<>&/]*\|/g,"")
 }
 
 app.use((req, res, next) => {
